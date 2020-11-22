6ce115ce90cc6b32dcecfbbc818e8ae741a5a94c
betrones <betronesdorg@gmail.com>
2020-11-22 17:30:45 +0300

    Now it works

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,6 +1,6 @@
 function makeActive1() {
 	document.getElementById('js-1').classList.toggle('-active')
-	setTimeout(document.getElementById('js-1').classList.toggle('-active'), 1000)
+	// setTimeout(document.getElementById('js-1').classList.toggle('-active'), 1000)
 }
 
 function removeActive1(){
@@ -9,7 +9,7 @@ function removeActive1(){
 
 function makeActive2() {
 	document.getElementById('js-1').classList.toggle('-active')
-	setTimeout(document.getElementById('js-1').classList.toggle('-active'), 1000)
+	// setTimeout(document.getElementById('js-1').classList.toggle('-active'), 1000)
 }
 
 function removeActive2(){
@@ -18,7 +18,7 @@ function removeActive2(){
 
 function makeActive3() {
 	document.getElementById('js-1').classList.toggle('-active')
-	setTimeout(document.getElementById('js-1').classList.toggle('-active'), 1000);
+	// setTimeout(document.getElementById('js-1').classList.toggle('-active'), 1000);
 }
 
 function removeActive3(){
@@ -27,21 +27,21 @@ function removeActive3(){
 
 function trafficControl(){
 makeActive1()
-makeActive2()
-makeActive3()
-// setTimeout(removeActive1, 2000);
-// setTimeout(clearInterval(), 2000);
+// makeActive2()
+// makeActive3()
+setTimeout(removeActive1, 2000);
+setTimeout(clearInterval(), 2000);
 
-// setTimeout(makeActive2, 1000);
-// setTimeout(removeActive2, 2000);
-// setTimeout(clearInterval(), 2000);
+setTimeout(makeActive2, 1000);
+setTimeout(removeActive2, 2000);
+setTimeout(clearInterval(), 2000);
 
-// setTimeout(makeActive3, 2000);
-// setTimeout(removeActive3, 3000);
-// setTimeout(clearInterval(), 2000);
+setTimeout(makeActive3, 2000);
+setTimeout(removeActive3, 3000);
+setTimeout(clearInterval(), 2000);
 
-// setTimeout(makeActive2, 3000)
-// setTimeout(removeActive2, 4000)
+setTimeout(makeActive2, 3000)
+setTimeout(removeActive2, 4000)
 }
 
 setInterval(trafficControl, 4000)
